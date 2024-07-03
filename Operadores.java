5c2626ddb57a3802e533b12e3d93f92470fee530
dcatori <caaatori@gmail.com>
2024-07-03 16:58:50 -0300

    Add files via upload

---
+++ Operadores.java
@@ -0,0 +1,33 @@
+public class Operadores { // declaração da classe Operadores
+    public static void main(String[] args) throws Exception { // declaração do metodo principal
+        System.out.println("Operador de atribuição: ="); // imprimir no terminal, o tipo de texto informado no paramentro
+        String texto = "Texto"; // declarando o tipo (String), nome (texto) e o valor da variavel ("Texto"), junto com o operador de atribuiçao =
+        System.out.println("String texto = \"Texto\";"); // imprimir no terminal, a declaração da variavel
+        System.out.println(texto); // imprimir no terminal, com o paramentro da varialvel, junto com o seu valor atribuido.
+
+        System.out.println("Operador de soma: +"); // imprimir no terminal, o tipo de texto informado no paramentro
+        int soma = 1 + 1; // declaração do tipo (int), nome (soma) e o valor da variavel (1 + 1), junto com o operador matematico +
+        System.out.println("int soma = 1 + 1;"); // imprimir no terminal, a declaração da variavel
+        System.out.println(soma);  // imprimir no terminal, com o paramentro da varialvel, junto com o seu valor atribuido.
+
+        System.out.println("Operador de subtração: -"); // imprimir no terminal, o tipo de texto informado no paramentro
+        int subtracao = 1 - 1; // declaração do tipo (int), nome (subtracao) e o valor da variavel (1 - 1), junto com o operador matematico -
+        System.out.println("int subtracao = 1 - 1;"); // imprimir no terminal, a declaração da variavel
+        System.out.println(subtracao); // imprimir no terminal, com o paramentro da varialvel, junto com o seu valor atribuido.
+
+        System.out.println("Operador de divisão: /"); // imprimir no terminal, o tipo de texto informado no paramentro
+        int divisao = 1 / 1; // declaração do tipo (int), nome (divisao) e o valor da variavel (1 / 1), junto com o operador matematico /
+        System.out.println("int divisao = 1 / 1;"); // imprimir no terminal, a declaração da variavel
+        System.out.println(divisao); // imprimir no terminal, com o paramentro da varialvel, junto com o seu valor atribuido.
+
+        System.out.println("Operador de multiplicação: *"); // imprimir no terminal, o tipo de texto informado no paramentro
+        int multiplicacao = 1 * 1; // declaração do tipo (int), nome (multiplicacao) e o valor da variavel (1 * 1), junto com o operador matematico *
+        System.out.println("int multiplicacao = 1 * 1;"); // imprimir no terminal, a declaração da variavel
+        System.out.println(multiplicacao); // imprimir no terminal, com o paramentro da varialvel, junto com o seu valor atribuido.
+
+        System.out.println("Operador de concatenação de strings: +"); // imprimir no terminal, o tipo de texto informado no paramentro
+        String concatenacao = "Texto 1 + " + "Texto 2"; // declaração do tipo (string), nome (concatenacao) e o valor da variavel (Texto 1 +, Texto 2) junto com a concatenacao
+        System.out.println("String concatenacao = \"Texto 1 + \" + \"Texto 2\";"); // imprimir no terminal, a declaração da variavel
+        System.out.println(concatenacao); // imprimir no terminal, com o paramentro da varialvel, junto com o seu valor atribuido.
+    } // encerramento do bloco de código - metodo principal
+} // encerramento do bloco de códigos da classe Operadores
