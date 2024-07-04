246ee41531e67ca1a369255f08500b1da0375161
dcatori <caaatori@gmail.com>
2024-07-04 15:13:00 -0300

    Add files via upload

---
+++ Variaveis.java
@@ -0,0 +1,30 @@
+public class  Variaveis { // declarando a classe Variaveis, e inicio do bloco de cógigos da classe
+
+    public static void main(String[] args) throws Exception { // declaração do metodo princial
+        
+        int varInt = 2;  // declaração da variavel varInt, do tipo int, recebendo o valor 2
+        System.out.println("Variabel do tipo: " + varInt); // imprimir no terminal o texto informado ("....")  concatenado com a varialvel
+    
+        Integer varInteger = 3; // declaração da variavel varInteger, do tipo Integer, recebendo o valor 3
+        System.out.println("Variabel do tipo: " + varInteger); // imprimir no terminal o texto informado ("....")  concatenado com a varialvel
+
+        String varString = "texto"; // declaração da variavel varString, do tipo String, recebendo o valor "texto"
+        System.out.println("Variabel do tipo: " + varString);  // imprimir no terminal o texto informado ("....")  concatenado com a varialvel
+
+        boolean varBoolean = true; // declaração da variavel varBoolean, do tipo boolean, recebendo o valor verdadeiro (true)
+        System.out.println("Variabel do tipo: " + varBoolean); // imprimir no terminal o texto informado ("....")  concatenado com a varialvel
+
+        byte varByte = 1; // declaração da variavel varByte, do tipo byte, recebendo o valor 1
+        System.out.println("Variabel do tipo: " + varByte); // imprimir no terminal o texto informado ("....")  concatenado com a varialvel
+
+        char varChar = 'r';  // declaração da variavel varChar, do tipo char, recebendo o valor de um caractere ('r')
+        System.out.println("Variabel do tipo: " + varChar); // imprimir no terminal o texto informado ("....")  concatenado com a varialvel
+
+        float varFloat = 19.99f;  // declaração da variavel varFloat, do tipo float, recebendo o valor com casas decimais (19.99f)
+        System.out.println("Variabel do tipo: " + varFloat); // imprimir no terminal o texto informado ("....")  concatenado com a varialvel
+
+        double varDouble = 2.2222; // declaração da variavel varDouble, do tipo double, recebendo o valor com casas decimais (2.2222)
+        System.out.println("Variabel do tipo: " + varDouble); // imprimir no terminal o texto informado ("....")  concatenado com a varialvel
+        
+    } // encerramento do bloco de codigo
+} // encerramento do bloco de codigo da classe
