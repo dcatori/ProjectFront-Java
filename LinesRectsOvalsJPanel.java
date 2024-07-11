11c29ff5684773b80f3348c4c8b8027e9b5db984
dcatori <caaatori@gmail.com>
2024-07-11 16:30:16 -0300

    Add files via upload

--- LinesRectsOvalsJPanel.java
+++ LinesRectsOvalsJPanel.java
@@ -20,16 +20,16 @@ public class LinesRectsOvalsJPanel extends JPanel
       g.setColor(Color.RED);
       g.drawLine(5, 30, 380, 30);
       g.setColor(Color.ORANGE);
-      g.setFont(new Font("Arial", Font.BOLD, 22));
+      g.setFont(new Font("Algerian", Font.BOLD, 22));
       g.drawString("Desafio", 150, 30);
 
       g.setColor(Color.BLUE);
       g.drawRect(5, 40, 90, 55);
-      g.setFont(new Font("Arial",Font.BOLD, 22));
+      g.setFont(new Font("Algerian",Font.BOLD, 22));
       g.drawString("Senac", 15, 75);
       g.fillRect(100, 40, 90, 55);
       g.setColor(Color.ORANGE);
-      g.setFont(new Font("Arial",Font.BOLD, 22));
+      g.setFont(new Font("Algerian",Font.BOLD, 22));
       g.drawString("Senac", 115, 75);
       
       
@@ -39,7 +39,7 @@ public class LinesRectsOvalsJPanel extends JPanel
       g.drawString("Senac", 205, 75);
       g.setColor(Color.BLACK);
       g.drawRoundRect(290, 40, 90, 55, 20, 20);
-      g.setFont(new Font("Arial",Font.BOLD, 22));
+      g.setFont(new Font("Algerian",Font.BOLD, 22));
       g.setColor(Color.GRAY);
       
       g.drawString("Senac", 300, 75);
@@ -48,23 +48,23 @@ public class LinesRectsOvalsJPanel extends JPanel
       g.setColor(Color.GREEN);   
       g.draw3DRect(5, 100, 90, 55, true);
       g.setColor(Color.GREEN);
-      g.setFont(new Font("Arial",Font.ITALIC, 22));
+      g.setFont(new Font("Algerian",Font.ITALIC, 22));
       g.drawString("Senac", 10, 135);
       g.fill3DRect(100, 100, 90, 55, false);
       g.setColor(Color.PINK);
-      g.setFont(new Font("Arial",Font.BOLD, 15));
+      g.setFont(new Font("Algerian",Font.BOLD, 20));
       g.drawString("Senac", 110, 135);
       
            
       g.setColor(Color.MAGENTA);
       g.drawOval(195, 100, 90, 55);
       g.setColor(Color.GRAY);
-      g.setFont(new Font("Arial",Font.BOLD, 50));
+      g.setFont(new Font("Algerian",Font.BOLD, 50));
       g.drawString("S", 220, 150);
       g.setColor(Color.MAGENTA);
       g.fillOval(290, 100, 90, 55);
       g.setColor(Color.WHITE);
-      g.setFont(new Font("Arial",Font.BOLD, 10));  
+      g.setFont(new Font("Algerian",Font.BOLD, 22));  
       g.drawString("Senac", 300, 135);  
         
       
