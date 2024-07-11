6814e030deb0d9c8d02fe2dc48e080ca7337ca30
dcatori <caaatori@gmail.com>
2024-07-11 16:54:37 -0300

    Add files via upload

--- LinesRectsOvalsJPanel.java
+++ LinesRectsOvalsJPanel.java
@@ -3,7 +3,6 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import javax.swing.JPanel;
-import javax.swing.plaf.FontUIResource;
 import java.awt.Font;
 
 //import javafx.scene.text.Font;
@@ -20,51 +19,51 @@ public class LinesRectsOvalsJPanel extends JPanel
       g.setColor(Color.RED);
       g.drawLine(5, 30, 380, 30);
       g.setColor(Color.ORANGE);
-      g.setFont(new Font("Algerian", Font.BOLD, 22));
+      g.setFont(new Font("Algerian", Font.BOLD, 20));
       g.drawString("Desafio", 150, 30);
 
       g.setColor(Color.BLUE);
       g.drawRect(5, 40, 90, 55);
-      g.setFont(new Font("Algerian",Font.BOLD, 22));
-      g.drawString("Senac", 15, 75);
+      g.setFont(new Font("Algerian",Font.BOLD, 50));
+      g.drawString("S", 35, 85);
       g.fillRect(100, 40, 90, 55);
       g.setColor(Color.ORANGE);
-      g.setFont(new Font("Algerian",Font.BOLD, 22));
-      g.drawString("Senac", 110, 75);
+      g.setFont(new Font("Algerian",Font.BOLD, 50));
+      g.drawString("E", 125, 85);
       
       
       g.setColor(Color.BLACK);
       g.fillRoundRect(195, 40, 90, 55, 50, 50);
       g.setColor(Color.GRAY);
-      g.drawString("Senac", 205, 75);
+      g.drawString("N", 225, 85);
       g.setColor(Color.BLACK);
       g.drawRoundRect(290, 40, 90, 55, 20, 20);
-      g.setFont(new Font("Algerian",Font.BOLD, 22));
+      g.setFont(new Font("Algerian",Font.BOLD, 50));
       g.setColor(Color.GRAY);
-      g.drawString("Senac", 300, 75);
+      g.drawString("A", 315, 85);
       
       
       g.setColor(Color.GREEN);   
       g.draw3DRect(5, 100, 90, 55, true);
       g.setColor(Color.GREEN);
-      g.setFont(new Font("Algerian",Font.ITALIC, 22));
-      g.drawString("Senac", 10, 135);
+      g.setFont(new Font("Algerian",Font.ITALIC, 50));
+      g.drawString("C", 30, 145);
       g.fill3DRect(100, 100, 90, 55, false);
       g.setColor(Color.PINK);
-      g.setFont(new Font("Algerian",Font.BOLD, 20));
-      g.drawString("Senac", 110, 135);
+      g.setFont(new Font("Algerian",Font.BOLD, 40));
+      g.drawString("JA", 115, 140);
       
            
       g.setColor(Color.MAGENTA);
       g.drawOval(195, 100, 90, 55);
       g.setColor(Color.GRAY);
-      g.setFont(new Font("Algerian",Font.BOLD, 50));
-      g.drawString("S", 220, 145);
+      g.setFont(new Font("Algerian",Font.BOLD, 40));
+      g.drawString("VA", 210, 145);
       g.setColor(Color.MAGENTA);
       g.fillOval(290, 100, 90, 55);
       g.setColor(Color.WHITE);
-      g.setFont(new Font("Algerian",Font.BOLD, 22));  
-      g.drawString("Senac", 300, 135);  
+      g.setFont(new Font("Algerian",Font.BOLD, 50));  
+      g.drawString("!!", 315, 140);  
         
       
    } 
